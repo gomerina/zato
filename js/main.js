dc59bcfdfa92b3a88ccae5f9f652d59df0645d2c
gomerina <KuzovkovSer52reg@yandex.ru>
2022-10-13 21:29:25 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,1 @@
+$(".metriki-tab__item").on("click",(function(){$(this).addClass("active").siblings().removeClass("active"),$(".metriki-tab__content").eq($(this).index()).addClass("active").siblings().removeClass("active")})),$(".burger").on("click",(function(){$(this).toggleClass("active"),$(".header__menu").toggleClass("active")}));
